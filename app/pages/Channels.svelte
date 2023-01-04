9cf9dda7037d686e8591e4c9238f89724cd01682
betaboon <betaboon@0x80.ninja>
2023-01-04 22:38:41 +0100

    feat: implement page to display channels

--- app/pages/Channels.svelte
+++ app/pages/Channels.svelte
@@ -1,6 +1,12 @@
 <script lang="ts">
   import { onMount } from "svelte";
-  import { title } from "../stores/app";
+
+  import { title, busy } from "../stores/app";
+  import { activePluginChannel0, activePluginChannel1 } from "../stores/device";
+
+  import Plugin from "../components/Plugin.svelte";
+
+  $: $busy = !($activePluginChannel0 && $activePluginChannel1);
 
   onMount(() => {
     $title = "Channels";
@@ -8,5 +14,10 @@
 </script>
 
 <page actionBarHidden={true}>
-  <label text="channels" />
+  <stackLayout>
+    <Plugin channel={0} />
+    {#if $activePluginChannel0}
+      <Plugin channel={1} disabled={$activePluginChannel0.isStereo} />
+    {/if}
+  </stackLayout>
 </page>
